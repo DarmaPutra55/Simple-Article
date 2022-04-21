c171e2b0148fbe35122700e0cfe43d1f40535dc6
DarmaPutra55 <igustiagunggededarmaputra28@gmail.com>
2022-04-21 13:50:51 +0800

    +Added font awesome. +Fixed the UI problem. +Make navbar collapsible. +Added button on each article. +Added 'Tambah' on navbar. Later on this will be used to acess input page for new page.

--- js/simple.js
+++ js/simple.js
@@ -16,14 +16,28 @@ const createArticleBox = (header, article) => {
     const mainBox = document.createElement('div');
     const mainHeader = document.createElement('div');
     const mainArticle = document.createElement('div');
+    const headerTextWrapper = document.createElement('div');
+    const headerButtonWrapper = document.createElement('div');
     const headerText = document.createElement('h1');
+    const headerButton = document.createElement('button');
+    const headerButtonIcon = document.createElement('i');
     const articleText = document.createElement('p');
+
     mainBox.classList.add('content-box');
     mainHeader.classList.add('content-header');
     mainArticle.classList.add('content-body');
+    headerTextWrapper.classList.add('content-header-text');
+    headerButtonWrapper.classList.add('content-header-button');
+    headerButtonIcon.classList.add('fa', 'fa-solid', 'fa-ellipsis-vertical','fa-2x' );
+
     headerText.innerHTML = header;
     articleText.innerHTML = article;
-    mainHeader.appendChild(headerText);
+
+    mainHeader.appendChild(headerTextWrapper);
+    mainHeader.appendChild(headerButtonWrapper);
+    headerTextWrapper.appendChild(headerText);
+    headerButton.appendChild(headerButtonIcon);
+    headerButtonWrapper.appendChild(headerButton);
     mainArticle.appendChild(articleText);
     mainBox.appendChild(mainHeader);
     mainBox.appendChild(mainArticle);
