6087d6373c059e43454d3426cbdf3e19023a5bf4
I Gusti Agung Gede Darma Putra <97772910+DarmaPutra55@users.noreply.github.com>
2022-11-04 20:30:00 -0700

    Update mailLogic.php

--- php/mail/mailLogic.php
+++ php/mail/mailLogic.php
@@ -31,7 +31,7 @@
                 $mail->isSMTP();                                            //Send using SMTP
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'absoluteneos@gmail.com';                     //SMTP username
+                $mail->Username   = 'yoursmtp.username';                     //SMTP username
                 $mail->Password   = 'ckmpnlsfzgsyktlf';                               //SMTP password
                 $mail->Port = 587;
                 //Set sistem enkripsi untuk menggunakan - ssl (deprecated) atau tls
@@ -39,7 +39,7 @@
                 //$mail->SMTPDebug = 3;                             //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                 //Recipients
-                $mail->setFrom("absoluteneos@gmail.com", 'Tester');
+                $mail->setFrom("yoursmtp.username", 'SenderName');
                 $mail->addReplyTo($this->emailSender);
                 $mail->addAddress('jojoisbizzare@gmail.com');               //Name is optional
 
